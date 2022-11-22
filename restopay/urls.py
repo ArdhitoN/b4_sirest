5d68157bd60053ac9667446b0623709dd05e9cc4
Shabrina Nurmalitasari <shabrina.nurmalitasari@ui.ac.id>
2022-11-22 10:34:48 +0700

    finished initial templates for restopay

--- restopay/urls.py
+++ restopay/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='restopay-index'),
+    path('isi/', views.isi, name='restopay-isi'),
+    path('tarik/', views.tarik, name='restopay-tarik'),
 ]
