80e3341e0efba20215eeefec2574626cdc2cf410
Shabrina Nurmalitasari <shabrina.nurmalitasari@ui.ac.id>
2022-11-22 15:02:51 +0700

    created jam_daftar jam_buat templates

--- jam_operasional/urls.py
+++ jam_operasional/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('buat/', views.jam_buat, name='restopay-index'),
+    path('buat/', views.jam_buat, name='jam-buat'),
+    path('daftar/', views.jam_daftar, name='jam-daftar'),
+    path('', views.jam_daftar, name='jam-daftar-index')
 ]
--- jam_operasional/views.py
+++ jam_operasional/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def jam_buat(response):
     return render(response, 'jam_buat.html')
+
+def jam_daftar(response):
+    return render(response, 'jam_daftar.html')
