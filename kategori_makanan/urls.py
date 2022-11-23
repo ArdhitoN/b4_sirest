06dfaa7d6334c14471d24b368eca190aff199af8
Kade Satrya <kade.satrya@ui.ac.id>
2022-11-24 02:57:58 +0700

    FoodCategory basic template and routing

--- kategori_makanan/urls.py
+++ kategori_makanan/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import *
+from kategori_makanan.views import *
 
 urlpatterns = [
     path('', show_kategori_makanan, name='show_kategori_makanan'),
+    path('tambah/', show_tambah_kategori_makanan, name='show_tambah_kategori_makanan')
 ]
--- kategori_makanan/views.py
+++ kategori_makanan/views.py
@@ -1,4 +1,9 @@
 from django.shortcuts import render
+from kategori_makanan.models import FoodCategory
 
 def show_kategori_makanan(request):
-    return render(request, 'kategori_makanan.html')
+    context = {"kategori_makanan": FoodCategory.objects.all()}
+    return render(request, 'kategori_makanan.html', context)
+
+def show_tambah_kategori_makanan(request):
+    return render(request, 'tambah_kategori_makanan.html')
