1d2922ab029bca324a3fe334fabde7b2049c9e6e
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-11-24 11:16:17 +0700

    update tarifPengiriman & Makanan app

--- Makanan/urls.py
+++ Makanan/urls.py
@@ -6,7 +6,13 @@ from .views import *
 app_name = 'Makanan'
 
 urlpatterns = [
-    path('lihat_makanan/', show_makanan, name='show_makanan'),
+    path('buat_makanan/', show_buat_makanan, name='show_buat_makanan'),
+    path('daftarMakanan_nonRestoView/', show_makanan_nonRestoView, name='show_makanan_nonRestoView'),
+    path('daftarMakanan_restoView/', show_makanan_restoView, name='show_makanan_restoView'),
+    path('detail_resto/', show_detail_resto, name='show_detail_resto'),
+    path('daftar_restoran/', show_daftar_restoran, name='show_daftar_restoran'),
+    path('ubah_makanan/', show_ubah_makanan, name='show_ubah_makanan'),
 
+    path('hapus_makanan/', hapus_makanan, name='hapus_makanan'),
 
 ]
--- tarifPengiriman/urls.py
+++ tarifPengiriman/urls.py
@@ -7,6 +7,9 @@ app_name = 'tarifPengiriman'
 
 urlpatterns = [
     path('buat_tarif/', show_buat_tarif, name='show_buat_tarif'),
+    path('daftar_tarif/', show_daftar_tarif, name='show_daftar_tarif'),
+    path('update_tarif/', show_update_tarif, name='show_update_tarif'),
+    path('hapus_tarif', hapus_tarif, name="hapus_tarif"),
 
 
 ]
