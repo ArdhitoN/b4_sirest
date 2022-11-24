83674b78b8541baa74393742f43ddf148e6af173
Kade Satrya <kade.satrya@ui.ac.id>
2022-11-24 11:48:43 +0700

    Added delete & create for foodcategory

--- kategori_makanan/urls.py
+++ kategori_makanan/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 from kategori_makanan.views import *
 
+app_name = "kategori_makanan"
+
 urlpatterns = [
     path('', show_kategori_makanan, name='show_kategori_makanan'),
-    path('tambah/', show_tambah_kategori_makanan, name='show_tambah_kategori_makanan')
+    path('tambah/', show_tambah_kategori_makanan, name='show_tambah_kategori_makanan'),
+    path('hapus/<int:pk>', delete_kategori_makanan, name='delete_kategori_makanan'),
 ]
