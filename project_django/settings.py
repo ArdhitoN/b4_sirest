bbd156ba1140eca705b6dba6cf4a975c41907dff
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-12-08 10:34:23 +0700

    test fix crsf verification error

--- project_django/settings.py
+++ project_django/settings.py
@@ -29,6 +29,8 @@ SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7
 DEBUG = True
 
 ALLOWED_HOSTS = ["*"]
+APP_NAME = "sirest-b4";
+CSRF_TRUSTED_ORIGINS = [f'https://{APP_NAME}.up.railway.app']
 
 # Application definition
 
