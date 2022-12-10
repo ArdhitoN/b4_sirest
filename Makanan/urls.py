7c430878a463595903b05713ae39bb0ad675edd1
ArdhitoNurhadyansah <ardhitonurhadyansah66@gmail.com>
2022-12-10 18:06:12 +0700

    update Makanan app for nonRestoUser

--- Makanan/urls.py
+++ Makanan/urls.py
@@ -10,16 +10,12 @@ urlpatterns = [
     path('daftarMakanan_restoView/', show_makanan_restoView, name='show_makanan_restoView'),
     path('ubah_makanan/', show_ubah_makanan, name='show_ubah_makanan'),
 
-    path('daftarMakanan_nonRestoView/', show_makanan_nonRestoView, name='show_makanan_nonRestoView'),
+    path('daftarMakanan_nonRestoView/<str:rname>/<str:rbranch>/', show_makanan_nonRestoView, name='show_makanan_nonRestoView'),
 
-    path('detail_resto/', show_detail_resto, name='show_detail_resto'),
-    path('detail_resto_NRView/', show_detail_resto_NRView, name='show_detail_resto_NRView'),
-
+    path('detail_resto/<str:rname>/<str:rbranch>/', show_detail_resto, name='show_detail_resto'),
     
     path('daftar_restoran/', show_daftar_restoran, name='show_daftar_restoran'),
-    path('daftar_restoran_NRView/', show_daftar_restoran_NRView, name='show_daftar_restoran_NRView'),
     
-
 
     path('hapus_makanan/', hapus_makanan, name='hapus_makanan'),
 
