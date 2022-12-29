649bb5f67df70ee878512abb4d44a8d5dbf30ba2
gilangarfadika04@gmail.com <gilangarfadika04@gmail.com>
2022-12-29 12:01:21 +0700

    baru7

--- project_django/settings.py
+++ project_django/settings.py
@@ -28,7 +28,7 @@ print(PRODUCTION)
 SECRET_KEY = 'django-insecure-3@5wx%(^*zl68l(o$^m-3%cvjv0g&mom1ra=oj5f048_al57s7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 APP_NAME = "sirest-b4";
